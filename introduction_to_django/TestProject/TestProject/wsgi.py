d0aed7a3fcb71f4d851fc970134bd406c99b0aff
Asen Kamboshev <lion963@mail.bg>
2021-04-22 12:59:51 +0300

    python_web_22_04

--- introduction_to_django/TestProject/TestProject/wsgi.py
+++ introduction_to_django/TestProject/TestProject/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for TestDjangoProject project.
+WSGI config for TestProject project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TestDjangoProject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TestProject.settings')
 
 application = get_wsgi_application()
