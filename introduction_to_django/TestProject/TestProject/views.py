0336f1ffccc4a2eec0d98a6919f7432581642109
Asen Kamboshev <lion963@mail.bg>
2021-04-27 14:30:21 +0300

    python_web_django_tests_27_04

---
+++ introduction_to_django/TestProject/TestProject/views.py
@@ -0,0 +1,27 @@
+from django.contrib.auth.models import User
+from django.shortcuts import render
+from django.views.generic import ListView
+
+
+def index(request):
+    title = 'SoftUni Django TestProject'
+    users = User.objects.all()
+    context = {
+        'title': title,
+        'users': users
+    }
+    return render(request, 'index.html', context)
+
+
+class UsersListView(ListView):
+    model = User
+    template_name = 'index2.html'
+    queryset = User.objects.all().order_by('-username')
+
+    def get_context_object_name(self, object_list):
+        return 'users'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data()
+        context['title'] = 'SoftUni Django TestProject - from ClassView'
+        return context
