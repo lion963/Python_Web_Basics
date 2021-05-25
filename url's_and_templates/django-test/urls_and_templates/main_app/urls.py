3fc1843641f8f4e620dcb361288297eb9effcbcc
Asen Kamboshev <lion963@mail.bg>
2021-05-25 09:37:15 +0300

    python_web_django_tests_25.05

---
+++ url's_and_templates/django-test/urls_and_templates/main_app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from main_app import views
+
+urlpatterns = [
+    path('', views.index)
+]
---
+++ url's_and_templates/django-test/urls_and_templates/secondary_app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from secondary_app import views
+
+urlpatterns = [
+    path('', views.index)
+]
