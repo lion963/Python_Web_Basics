00deb4894282021d4320473649b23e41d8a6db33
Asen Kamboshev <lion963@mail.bg>
2021-06-03 13:40:08 +0300

    exam_preparation_RECIPES_03.06

--- exam_preparation/Recipes/Recipes/urls.py
+++ exam_preparation/Recipes/Recipes/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from Recipes.views import home_page, create_recipe, delete_recipe
+from Recipes.views import home_page, create_recipe, delete_recipe, edit_recipe, details_recipe
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_page),
     path('create', create_recipe),
     path('delete', delete_recipe),
+    path('edit', edit_recipe),
+    path('details', details_recipe)
 ]
--- exam_preparation/Recipes/Recipes/views.py
+++ exam_preparation/Recipes/Recipes/views.py
@@ -1,15 +1,29 @@
 from django.shortcuts import render
 from Recipes.models import Recipe
 
+
 def home_page(request):
+    # try:
+    #     recipes_data = Recipe.objects.get()
+    # except:
+    #     recipes_data = False
     context = {
         'recipes': True
     }
     return render(request, 'index.html', context)
 
+
 def create_recipe(request):
     return render(request, 'create.html')
+
 
 def delete_recipe(request):
     return render(request, 'delete.html')
 
+
+def edit_recipe(request):
+    return render(request, 'edit.html')
+
+
+def details_recipe(request):
+    return render(request, 'details.html')
