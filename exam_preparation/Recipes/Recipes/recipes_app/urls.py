8282130375ba1022593bd58503b44ddb02bf30eb
Asen Kamboshev <lion963@mail.bg>
2021-06-07 11:42:06 +0300

    exam_preparation_RECIPES_07.06

--- exam_preparation/Recipes/Recipes/recipes_app/urls.py
+++ exam_preparation/Recipes/Recipes/recipes_app/urls.py
@@ -4,7 +4,6 @@ from django.urls import path
 from Recipes.recipes_app.views import index, create_recipe, delete, edit, details
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', index),
     path('create', create_recipe),
     path('delete', delete),
